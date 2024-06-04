6832f0f09d977fdc1c1a9824658f2175cb90d1a8
ziimakc <ziimakc@gmail.com>
2024-06-05 00:17:45 +0300

    chore: types issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,19 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { type PropsOf, component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
     <>
       <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
     </>
   );
 });
 
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
+type InputProps = PropsOf<"input">;
+
+export const InputBase = component$<InputProps>((props) => {
+  return <input {...props} />;
+});
+
+export const InputWrapper = component$<InputProps>((props) => {
+  return <InputBase autoComplete="email" {...props} />;
+});
